dee439466280a96c07019f0bfa12a241878917d5
TecnoLògics Bellvitge <tecnologics@ibellvitge.net>
2022-07-30 21:19:28 +0200

    Update nivell_iluminacio.py

--- nivell_iluminacio.py
+++ nivell_iluminacio.py
@@ -7,10 +7,10 @@ from time import sleep
 # definició d'objectes
 led = PWM (Pin(23), freq=5000)
 sensor = ADC (Pin(32))
-sensor.width (ADC.WIDTH_10BIT)  # per obtenir valors entre 0 i 1023
+sensor.width (ADC.WIDTH_10BIT)      # per obtenir valors entre 0 i 1023
 
 # execució contínua
 while True:
-  valor_sensor = sensor.read()  # llegeix l'entrada analògica (valors entre 0 i 1023)
-  led.duty (valor_sensor)       # assigna nivell d'il·luminació (0 a 1023) segons valor sensor (0 a 1023)
+  valor_sensor = sensor.read()      # llegeix l'entrada analògica (valors entre 0 i 1023)
+  led.duty (valor_sensor)           # assigna nivell d'il·luminació (0 a 1023) segons valor sensor (0 a 1023)
   sleep(.1)
