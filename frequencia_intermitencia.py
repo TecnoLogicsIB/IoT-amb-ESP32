a3d7dd10d9bef62bd1e0c6d1779f5573ea6942a1
TecnoLògics Bellvitge <tecnologics@ibellvitge.net>
2022-07-30 21:14:58 +0200

    Update frequencia_intermitencia.py

--- frequencia_intermitencia.py
+++ frequencia_intermitencia.py
@@ -7,13 +7,13 @@ from time import sleep
 # definició d'objectes
 led = Pin(23, Pin.OUT)
 sensor = ADC (Pin(32))
-sensor.width (ADC.WIDTH_10BIT)  # ajust de la resolució per obtenir valors entre 0 i 1023
+sensor.width (ADC.WIDTH_10BIT)      # ajust de la resolució per obtenir valors entre 0 i 1023
 
 # execució contínua
 while True:
-  valor_sensor = sensor.read()  # llegeix l'entrada analògica (valors entre 0 i 1023)
-  pausa = valor_sensor/1000     # divideixo entre 1000 perquè la pausa ha de ser en segons
-  led.on()                      # encén el led
-  sleep(pausa)                  # durant el temps corresponent a la lectura del sensor
-  led.off()                     # apaga el led
+  valor_sensor = sensor.read()      # llegeix l'entrada analògica (valors entre 0 i 1023)
+  pausa = valor_sensor/1000         # divideixo entre 1000 perquè la pausa ha de ser en segons
+  led.on()                          # encén el led
+  sleep(pausa)                      # durant el temps corresponent a la lectura del sensor
+  led.off()                         # apaga el led
   sleep(pausa)
