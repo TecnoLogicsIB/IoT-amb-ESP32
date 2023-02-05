9e0fca318f8524007a625a63265c1fa6c415a325
TecnoLògics Bellvitge <tecnologics@ibellvitge.net>
2023-02-05 07:53:39 +0100

    Update DS18B20_1.py

--- DS18B20_1.py
+++ DS18B20_1.py
@@ -6,17 +6,17 @@ from machine import Pin    # per interactuar amb els GPIO
 from time import sleep     # per definir pauses
 
 # creació d'objecte ds18x20 associat al pin de connexió del sensor:
-ds_sensor = ds18x20.DS18X20(onewire.OneWire(Pin(4)))
+sensor = ds18x20.DS18X20(onewire.OneWire(Pin(4)))
 
-# cerca l'objecte definit i desa la seva adreça dins la variable roms, que té format de llista:
-roms = ds_sensor.scan()                  
-print('Trobat dispositiu DS: ', roms)    # mostra l'adreça trobada
+# cerca l'objecte definit i desa la seva adreça dins la variable adreces, que té format de llista:
+adreces = sensor.scan()                  
+print('Trobat dispositiu DS: ', adreces)    # mostra l'adreça o adreces trobades
 
 while True:
-    ds_sensor.convert_temp()  # llegeix el sensor i converteix les dades
+    sensor.convert_temp()     # llegeix el sensor i converteix les dades
     sleep (0.75)              # cal donar temps a realitzar l'acció anterior
-    for rom in roms:          # per a cada element de la llista roms ...
-        #print (rom)          # això mostra l'adreça del dispositiu ds_sensor
-        #print (ds_sensor.read_temp(rom))             # mostra la temperatura
-        print ('T=', ds_sensor.read_temp(rom), 'ºC')  # mostra la temperatura: magnitud, valor i unitat
+    for i in adreces:         # per a cada element de la llista roms ...
+        #print (i)            # això mostra l'adreça del dispositiu ds_sensor
+        #print (sensor.read_temp(i))             # mostra la temperatura
+        print ('T=', sensor.read_temp(i), 'ºC')  # mostra la temperatura: magnitud, valor i unitat
     sleep (5)                 # interval d'actualització: 5 segons 
