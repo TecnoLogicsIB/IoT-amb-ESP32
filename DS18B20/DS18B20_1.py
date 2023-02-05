05a8a14d33813eb20da86fe84db069a5a5855548
TecnoLògics Bellvitge <tecnologics@ibellvitge.net>
2023-02-05 08:54:14 +0100

    Add files via upload
    Mostra la temperatura de només 1 sensor DS18B20

---
+++ DS18B20/DS18B20_1.py
@@ -0,0 +1,20 @@
+# importa mòduls:
+import onewire, ds18x20    # per interactuar amb el sensor DS18B20
+from machine import Pin    # per interactuar amb els GPIO
+from time import sleep     # per definir pauses
+
+# creació d'objecte ds18x20 associat al pin de connexió del sensor:
+sensor = ds18x20.DS18X20(onewire.OneWire(Pin(4)))
+
+# cerca l'objecte definit i desa la seva adreça dins la variable adreça, que té format de llista:
+adreça = sensor.scan()                  
+print('Trobat dispositiu DS: ', adreça)    # mostra l'adreça trobada
+
+while True:
+    sensor.convert_temp()     # llegeix el sensor i converteix les dades
+    sleep (0.75)              # cal donar temps a realitzar l'acció anterior
+    # variable per desar la temperatura llegida per cada sensor:
+    temperatura = sensor.read_temp(adreça[0])
+    # mostra les temperatures (magnitud, valor i unitat):
+    print ('T=', temperatura, 'ºC')      
+    sleep (5)                 # interval d'actualització: 5 segons
