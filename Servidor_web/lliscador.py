7d8847fb11b2e872df7ceb7419d743424fb10590
TecnoLògics Bellvitge <tecbell@gmail.com>
2023-08-25 19:14:06 +0200

    Update lliscador.py

--- Servidor_web/lliscador.py
+++ Servidor_web/lliscador.py
@@ -11,11 +11,13 @@ servidor.listen(5)
 def pag_web():
     html="""<html>
 <head>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<title>Servidor PWM</title>
+<title>ESP32 Servidor Web</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 </head>
 <body>
+<h1>Mi Servidor</h1>
+<p>Hola, """ + addr[0] + """</p>
 <p>LLUMINOSITAT LED: <span id="textSliderValue">50</span> %</p>
 <p><input type="range" onchange="updateSliderPWM(this)" id="pwmSlider" min="0" max="100" step="1" class="slider"></p>
 <script>
